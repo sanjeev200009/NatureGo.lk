ae1e94744622e55f6361e08f540efdc89d7d31d3
sanjeev200009 <sanjaysanjeev2000@gmail.com>
2025-06-08 15:36:58 +0530

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/NatureGo.lk/' : '/',
-  server: {
-    host: "::",
-    port: 8000,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: "/NatureGo.lk/",
+});
